19d19ea4252164935d6d33d4e255c8e37f476ce8
Dimitrios Kalanidis <dimitriskaln@gmail.com>
2025-02-18 17:10:32 +0100

    Second Commit

--- src/main.java
+++ src/main.java
@@ -5,6 +5,7 @@ public class main {
     public static void main(String[] args) {
         System.out.println("First Commit Test");
         System.out.println("Second Commit Test");
+        System.out.println("Third Commit Test");
     }
 
 }
