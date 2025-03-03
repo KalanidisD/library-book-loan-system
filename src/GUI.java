1e15f4c389ba6f7ebcf8763683d15de2a140d522
KalanidisD <dimitriskaln@gmail.com>
2025-03-03 19:49:36 +0100

    trying to print query to jtable

--- src/GUI.java
+++ src/GUI.java
@@ -7,23 +7,18 @@ import javax.swing.table.JTableHeader;
 import java.awt.*;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.Vector;
+
 
 public class GUI {
 
+
+
     public static void mainFrame() {
-        Object [][]data={{"John,", 1, 1, 1, 1, 1},
-                         {"Jim",2,2,2,2,2},
-                         {"Jim",2,2,2,2,2},
-                         {"John,", 1, 1, 1, 1, 1},
-                         {"Jim",2,2,2,2,2},
-                         {"Jim",2,2,2,2,2},
-
-
-        };
-        String []tableColumns={"id","username","password","firstname","lastname","email"};
-
-        DefaultTableModel model = new DefaultTableModel(data, tableColumns);
+        Vector data = new Vector();
+        Vector columnNames=new Vector();
 
         JFrame frame = new JFrame("Library Book Loan System"); // create item frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);       //frame settings
@@ -41,16 +36,49 @@ public class GUI {
 
         JButton resultsButton = new JButton("Show results");   //create  results button
         resultsButton.setFocusable(false);
-        resultsButton.setBounds(1300,600,150,100);
+        resultsButton.setBounds(800,600,150,100);
 
+
+        resultsButton.addActionListener(e -> {
+            try {
+                String query = "SELECT * FROM users";
+                DatabaseConnection.StartConnection(); //establish connection
+                PreparedStatement stmt = DatabaseConnection.conn.prepareStatement(query); //prepare the statement
+
+                ResultSet rs = stmt.executeQuery();
+                ResultSetMetaData rsmd = rs.getMetaData();
+                int columns=rsmd.getColumnCount();
+
+                while (rs.next()) {
+                    Vector row = new Vector(columns);
+                    for(int i=1; i<= columns; i++) {
+                        row.add(rs.getObject(i));
+                    }
+                    data.add(row);
+                }
+                rs.close();
+                stmt.close();
+            }
+            catch(SQLException r){
+
+                //if false
+                System.out.println("Connection failed "+r.getMessage());
+            }
+        });
+
+        DefaultTableModel model = new DefaultTableModel(data,columnNames );
         JTable usersTable = new JTable(model);    //create user Table
+        usersTable.setBackground(Color.DARK_GRAY);
+        usersTable.setForeground(Color.WHITE);
+        usersTable.setFont(new Font("ITALIC", Font.BOLD, 15));
         usersTable.setShowGrid(true);
         usersTable.setGridColor(Color.black);
         usersTable.setBorder(BorderFactory.createLineBorder(Color.black));
         usersTable.setBounds(0,0,400,499);
 
+
         JScrollPane userPane = new JScrollPane(usersTable);
-        userPane.setBorder(BorderFactory.createLineBorder(Color.red));
+        userPane.setBorder(BorderFactory.createLineBorder(Color.darkGray));
         userPane.setBounds(0,0,400,500);
 
         JLabel resultsLabel = new JLabel("Users");
@@ -59,6 +87,7 @@ public class GUI {
         resultsLabel.setBounds(200,0,100,45);
 
         mainPanel.add(userPane);
+        frame.add(resultsButton);
         frame.add(resultsLabel);
         frame.add(mainPanel);
 
